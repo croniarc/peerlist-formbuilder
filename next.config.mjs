5f40b7ae032acea9f660bcb629404afeb9f3053e
croniarc <croniarc@gmail.com>
2024-12-10 23:40:43 +0530

    code pushed

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-    output: 'export',
-    // If your repository name is 'my-repo', set basePath to '/my-repo'
-    basePath: '/peerlist-formbuilder',
-};
+const nextConfig = {};
 
 export default nextConfig;
