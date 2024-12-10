46f165942e6044ea93864bee53fc3da27f545d72
croniarc <croniarc@gmail.com>
2024-12-10 23:19:53 +0530

    commit wit hworking functionalities

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: 'export',
+    // If your repository name is 'my-repo', set basePath to '/my-repo'
+    basePath: '/peerlist-formbuilder',
+};
 
 export default nextConfig;
